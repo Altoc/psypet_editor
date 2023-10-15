8aa4631abb44ca3050e7b9ba0b89800416a7d7fc
Ian Whitesel <altocgamedev@gmail.com>
2023-10-15 12:12:59 -0400

    Dialogue line editor done??

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import sqlite3
 class DialogueLine:
     def __init__(self, sequence_id, text, next_seq_id, accept_seq_id, decline_seq_id, challenge_id, mission_start_id, mission_condition_update_id):
         self.sequence_id = sequence_id
-        self.text = text
-        self.next_seq_id = next_seq_id
-        self.accept_seq_id = accept_seq_id
-        self.decline_seq_id = decline_seq_id
-        self.challenge_id = challenge_id
-        self.mission_start_id = mission_start_id
-        self.mission_condition_update_id = mission_condition_update_id
+        self.text = text if text != "" else None
+        self.next_seq_id = next_seq_id if next_seq_id != "" else None
+        self.accept_seq_id = accept_seq_id if accept_seq_id != "" else None
+        self.decline_seq_id = decline_seq_id if decline_seq_id != "" else None
+        self.challenge_id = challenge_id if challenge_id != "" else None
+        self.mission_start_id = mission_start_id if mission_start_id != "" else None
+        self.mission_condition_update_id = mission_condition_update_id if mission_condition_update_id != "" else None
 
 
 # Custom class to represent a set of user-input on dialogue editor
@@ -82,8 +82,8 @@ def open_insert_record_window():
         # Here's a sample insert query:
         for record in dialogue_lines:
             query = "INSERT INTO DIALOGUE_dialogue_line (set_id, seq_id, text) VALUES", str(set_id), str(record.sequence_id), str(record.text)
-            cursor.execute("INSERT INTO DIALOGUE_dialogue_line (set_id, seq_id, text) VALUES (?, ?, ?)",
-                           (set_id, record.sequence_id, record.text))
+            cursor.execute("INSERT INTO DIALOGUE_dialogue_line (set_id, seq_id, next_seq_id, accept_next_seq_id, decline_next_seq_id, text, challenge_id, mission_start_id, mission_condition_update_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                           (set_id, record.sequence_id, record.next_seq_id, record.accept_seq_id, record.decline_seq_id, record.text, record.challenge_id, record.mission_start_id, record.mission_condition_update_id))
             print(query)
         conn.commit()
         conn.close()
@@ -103,31 +103,19 @@ def open_insert_record_window():
                                    ttk.Entry(insert_window))
         text_entries.append(text_entry)
 
-        text_label = ttk.Label(insert_window, text="Text:")
-        next_seq_label = ttk.Label(insert_window, text="next seq id:")
-        accept_seq_label = ttk.Label(insert_window, text="accept seq id:")
-        decline_seq_label = ttk.Label(insert_window, text="decline seq id:")
-        challenge_id_label = ttk.Label(insert_window, text="challenge id:")
-        mission_start_id_label = ttk.Label(insert_window, text="mission start id:")
-        mission_condition_update_id_label = ttk.Label(insert_window, text="mission condition update id:")
+        seq_id_value = ttk.Label(insert_window, text=str(sequence_id))
 
-        text_entry.text_entry.grid(row=sequence_id, column=1)
-        text_entry.next_seq_entry.grid(row=sequence_id, column=3)
-        text_entry.accept_seq_entry.grid(row=sequence_id, column=5)
-        text_entry.decline_seq_entry.grid(row=sequence_id, column=7)
-        text_entry.challenge_entry.grid(row=sequence_id, column=9)
-        text_entry.mission_start_entry.grid(row=sequence_id, column=11)
-        text_entry.mission_condition_update_entry.grid(row=sequence_id, column=13)
+        seq_id_value.grid(row=sequence_id + 1, column=0)
+        text_entry.text_entry.grid(row=sequence_id + 1, column=1)
+        text_entry.next_seq_entry.grid(row=sequence_id + 1, column=2)
+        text_entry.accept_seq_entry.grid(row=sequence_id + 1, column=3)
+        text_entry.decline_seq_entry.grid(row=sequence_id + 1, column=4)
+        text_entry.challenge_entry.grid(row=sequence_id + 1, column=5)
+        text_entry.mission_start_entry.grid(row=sequence_id + 1, column=6)
+        text_entry.mission_condition_update_entry.grid(row=sequence_id + 1, column=7)
 
-        text_label.grid(row=sequence_id, column=0, sticky=tk.W)
-        next_seq_label.grid(row=sequence_id, column=2, sticky=tk.W)
-        accept_seq_label.grid(row=sequence_id, column=4, sticky=tk.W)
-        decline_seq_label.grid(row=sequence_id, column=6, sticky=tk.W)
-        challenge_id_label.grid(row=sequence_id, column=8, sticky=tk.W)
-        mission_start_id_label.grid(row=sequence_id, column=10, sticky=tk.W)
-        mission_condition_update_id_label.grid(row=sequence_id, column=12, sticky=tk.W)
-        submit_button.grid(row=sequence_id + 2, column=0, columnspan=2)
-        add_line_button.grid(row=sequence_id + 2, column=3, columnspan=2)
+        submit_button.grid(row=sequence_id + 3, column=0, columnspan=2)
+        add_line_button.grid(row=sequence_id + 3, column=3, columnspan=2)
 
     insert_window = Toplevel(root)
     insert_window.title("Insert Record")
@@ -145,33 +133,38 @@ def open_insert_record_window():
                                ttk.Entry(insert_window))
     text_entries.append(text_entry)
 
+    seq_id_label = ttk.Label(insert_window, text="seq id:")
+    seq_id_value = ttk.Label(insert_window, text=str(sequence_id))
     text_label = ttk.Label(insert_window, text="Text:")
     next_seq_label = ttk.Label(insert_window, text="next seq id:")
     accept_seq_label = ttk.Label(insert_window, text="accept seq id:")
     decline_seq_label = ttk.Label(insert_window, text="decline seq id:")
     challenge_id_label = ttk.Label(insert_window, text="challenge id:")
     mission_start_id_label = ttk.Label(insert_window, text="mission start id:")
-    mission_condition_update_id_label = ttk.Label(insert_window, text="mission condition update id:")
+    mission_condition_update_id_label = ttk.Label(insert_window, text="mission cond update id:")
     submit_button = ttk.Button(insert_window, text="Submit", command=submit_record)
     add_line_button = ttk.Button(insert_window, text="Add Line", command=add_text_entry)
 
-    text_entry.text_entry.grid(row=sequence_id, column=1)
-    text_entry.next_seq_entry.grid(row=sequence_id, column=3)
-    text_entry.accept_seq_entry.grid(row=sequence_id, column=5)
-    text_entry.decline_seq_entry.grid(row=sequence_id, column=7)
-    text_entry.challenge_entry.grid(row=sequence_id, column=9)
-    text_entry.mission_start_entry.grid(row=sequence_id, column=11)
-    text_entry.mission_condition_update_entry.grid(row=sequence_id, column=13)
+    seq_id_value.grid(row=sequence_id+1, column=0)
+    text_entry.text_entry.grid(row=sequence_id+1, column=1)
+    text_entry.next_seq_entry.grid(row=sequence_id+1, column=2)
+    text_entry.accept_seq_entry.grid(row=sequence_id+1, column=3)
+    text_entry.decline_seq_entry.grid(row=sequence_id+1, column=4)
+    text_entry.challenge_entry.grid(row=sequence_id+1, column=5)
+    text_entry.mission_start_entry.grid(row=sequence_id+1, column=6)
+    text_entry.mission_condition_update_entry.grid(row=sequence_id+1, column=7)
 
-    text_label.grid(row=sequence_id, column=0, sticky=tk.W)
-    next_seq_label.grid(row=sequence_id, column=2, sticky=tk.W)
-    accept_seq_label.grid(row=sequence_id, column=4, sticky=tk.W)
-    decline_seq_label.grid(row=sequence_id, column=6, sticky=tk.W)
-    challenge_id_label.grid(row=sequence_id, column=8, sticky=tk.W)
-    mission_start_id_label.grid(row=sequence_id, column=10, sticky=tk.W)
-    mission_condition_update_id_label.grid(row=sequence_id, column=12, sticky=tk.W)
-    submit_button.grid(row=2, column=0, columnspan=2)
-    add_line_button.grid(row=2, column=3, columnspan=2)
+    seq_id_label.grid(row=0, column=0, sticky=tk.W)
+    text_label.grid(row=0, column=1, sticky=tk.W)
+    next_seq_label.grid(row=0, column=2, sticky=tk.W)
+    accept_seq_label.grid(row=0, column=3, sticky=tk.W)
+    decline_seq_label.grid(row=0, column=4, sticky=tk.W)
+    challenge_id_label.grid(row=0, column=5, sticky=tk.W)
+    mission_start_id_label.grid(row=0, column=6, sticky=tk.W)
+    mission_condition_update_id_label.grid(row=0, column=7, sticky=tk.W)
+
+    submit_button.grid(row=sequence_id+3, column=0, columnspan=2)
+    add_line_button.grid(row=sequence_id+3, column=3, columnspan=2)
 
 
 
