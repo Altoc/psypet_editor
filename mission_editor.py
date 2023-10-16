f2bb16c9f1c9b1776accb627775b7b8238604345
Ian Whitesel <altocgamedev@gmail.com>
2023-10-15 20:04:51 -0400

    mission editor complete

--- mission_editor.py
+++ mission_editor.py
@@ -2,44 +2,6 @@ import tkinter as tk
 from tkinter import ttk, Toplevel
 import db_util as db
 
-
-###--- DB CLASSES ---###
-
-# Custom class to represent a MISSION_mission record
-class Mission:
-    def __init__(self, mission_id, name, description):
-        self.mission_id = mission_id
-        self.name = name
-        self.description = description
-
-
-# Custom class to represent a MISSION_condition record
-class MissionCondition:
-    def __init__(self, condition_id, mission_id, name, mission_completer_flag):
-        self.condition_id = condition_id
-        self.mission_id = mission_id
-        self.name = name
-        self.mission_completer_flag = mission_completer_flag
-
-
-# Custom class to represent a MISSION_condition_prerequisite record
-class MissionConditionPrerequisite:
-    def __init__(self, condition_prereq_id, mission_id, prereq_of_id, prereq_to_id):
-        self.condition_prereq_id = condition_prereq_id
-        self.mission_id = mission_id
-        self.prereq_of_id = prereq_of_id
-        self.prereq_to_id = prereq_to_id
-
-
-# Custom class to represent a MISSION_condition_world_state record
-class MissionConditionWorldState:
-    def __init__(self, condition_world_state_id, condition_entity_id, world_state_id, active_flag):
-        self.condition_world_state_id = condition_world_state_id
-        self.condition_entity_id = condition_entity_id
-        self.world_state_id = world_state_id
-        self.active_flag = active_flag
-
-
 ###--- TK UI CLASSES ---###
 
 # Custom class to represent a set of user-input on the MISSION portion of the editor
@@ -51,7 +13,8 @@ class MissionEntry:
 
 # Custom class to represent a set of user-input on the MISSION portion of the editor
 class MissionConditionEntry:
-    def __init__(self, name_entry, mission_completer_check_var, mission_completer_entry, unlocked_conditions_id_entry, unlocked_world_states_id_entry):
+    def __init__(self, condition_id, name_entry, mission_completer_check_var, mission_completer_entry, unlocked_conditions_id_entry, unlocked_world_states_id_entry):
+        self.condition_id = condition_id
         self.name_entry = name_entry
         self.mission_completer_check_var = mission_completer_check_var
         self.mission_completer_entry = mission_completer_entry
@@ -64,23 +27,33 @@ class MissionConditionEntry:
 def open_mission_editor_window(root):
     def submit_record():
         nonlocal row_seq_id
+        nonlocal mission_id
 
         conn = db.connect_to_database()
         cursor = conn.cursor()
 
         # Create MISSION_mission record
-        cursor.execute("SELECT MAX(mission_id) FROM MISSION_mission;")
-        mission_id = int(cursor.fetchone()[0]) + 1
         cursor.execute("INSERT INTO MISSION_mission (mission_id, name, description) values (?, ?, ?)",
                        (mission_id, mission_entry.name_entry.get(), mission_entry.description_entry.get()))
 
-        # Create MISSION_condition records
-        cursor.execute("SELECT MAX(condition_id) FROM MISSION_condition;")
-        condition_id = int(cursor.fetchone()[0])
+        # Create MISSION_condition, MISSION_condition_prerequisite, and MISSION_condition_world_state records
         for input in condition_entry_widgets:
-            condition_id += 1
             cursor.execute("INSERT INTO MISSION_condition (condition_id, mission_id, name, complete_mission_flag) values (?, ?, ?, ?)",
-                           (condition_id, mission_id, input.name_entry.get(), input.mission_completer_check_var.get()))
+                           (input.condition_id, mission_id, input.name_entry.get(), input.mission_completer_check_var.get()))
+            #parse the supplied prereq ids, if not empty, run the prereq query
+            prereq_ids = [int(value) for value in input.unlocked_conditions_id_entry.get().split(',') if value.strip()]
+            for prereq in prereq_ids:
+                cursor.execute("INSERT INTO MISSION_condition_prerequisite (mission_id, prerequisite_of_id, prerequisite_to_id) values (?, ?, ?)",
+                               (mission_id, prereq, input.condition_id))
+            #parse the supplied world state ids, if not empty, run the query
+            pairs = input.unlocked_world_states_id_entry.split(',')
+            world_state_dict = {}
+            for pair in pairs:
+                key, value = map(int, pair.split(':'))
+                world_state_dict[key] = value
+            for key, value in world_state_dict.items():
+                cursor.execute("INSERT INTO MISSION_condition_world_state (condition_entity_id, world_state_id, active_flag) values (?, ?, ?)",
+                               (input.condition_id, key, value))
 
         conn.commit()
         conn.close()
@@ -89,11 +62,14 @@ def open_mission_editor_window(root):
 
     def add_condition_entry():
         nonlocal row_seq_id
+        nonlocal condition_id
         row_seq_id += 7
+        condition_id += 1
         separator = ttk.Separator(mission_editor_window, orient="horizontal")
 
         mission_completer_check_var = tk.IntVar()
-        condition_entry = MissionConditionEntry(ttk.Entry(mission_editor_window),
+        condition_entry = MissionConditionEntry(condition_id,
+                                                ttk.Entry(mission_editor_window),
                                                 mission_completer_check_var,
                                                 ttk.Checkbutton(mission_editor_window, text="Completes Mission", variable=mission_completer_check_var),
                                                 ttk.Entry(mission_editor_window),
@@ -101,10 +77,10 @@ def open_mission_editor_window(root):
         condition_entry_widgets.append(condition_entry)
 
         cond_id_label = ttk.Label(mission_editor_window, text="ID:")
-        cond_id_value = ttk.Label(mission_editor_window, text="[ID HERE]")
+        cond_id_value = ttk.Label(mission_editor_window, text=condition_id)
         cond_name_label = ttk.Label(mission_editor_window, text="Cond Name:")
-        prereq_ids_label = ttk.Label(mission_editor_window, text="Condition Unlocks:")
-        world_state_ids_label = ttk.Label(mission_editor_window, text="World State Unlocks:")
+        prereq_ids_label = ttk.Label(mission_editor_window, text="Condition Unlocks (comma delimited):")
+        world_state_ids_label = ttk.Label(mission_editor_window, text="World State Unlocks (state:active_flag, comma delimited):")
 
         separator.grid(row=row_seq_id + 2, column=0, columnspan=10, sticky="ew")
 
@@ -125,6 +101,15 @@ def open_mission_editor_window(root):
     mission_editor_window = Toplevel(root)
     mission_editor_window.title("Psypet Mission Editor")
     row_seq_id = 0
+
+    conn = db.connect_to_database()
+    cursor = conn.cursor()
+    cursor.execute("SELECT MAX(mission_id) FROM MISSION_mission;")
+    mission_id = int(cursor.fetchone()[0]) + 1
+    cursor.execute("SELECT MAX(condition_id) FROM MISSION_condition;")
+    condition_id = int(cursor.fetchone()[0]) + 1
+    conn.close()
+
     condition_entry_widgets = []  # List to store mission condition entry widgets
 
     # Widget Creation:
@@ -135,13 +120,14 @@ def open_mission_editor_window(root):
     submit_button = ttk.Button(mission_editor_window, text="Submit", command=submit_record)
     add_cond_button = ttk.Button(mission_editor_window, text="Add Condition", command=add_condition_entry)
     mission_id_label = ttk.Label(mission_editor_window, text="ID:")
-    mission_id_value = ttk.Label(mission_editor_window, text="[ID HERE]")
+    mission_id_value = ttk.Label(mission_editor_window, text=mission_id)
     mission_name_label = ttk.Label(mission_editor_window, text="Mission Name:")
     mission_descr_label = ttk.Label(mission_editor_window, text="Description:")
     separator = ttk.Separator(mission_editor_window, orient="horizontal")
 
     mission_completer_check_var = tk.IntVar()
-    condition_entry = MissionConditionEntry(ttk.Entry(mission_editor_window),
+    condition_entry = MissionConditionEntry(condition_id,
+                                            ttk.Entry(mission_editor_window),
                                             mission_completer_check_var,
                                             ttk.Checkbutton(mission_editor_window, text="Completes Mission", variable=mission_completer_check_var),
                                             ttk.Entry(mission_editor_window),
@@ -149,10 +135,10 @@ def open_mission_editor_window(root):
     condition_entry_widgets.append(condition_entry)
 
     cond_id_label = ttk.Label(mission_editor_window, text="ID:")
-    cond_id_value = ttk.Label(mission_editor_window, text="[ID HERE]")
+    cond_id_value = ttk.Label(mission_editor_window, text=condition_id)
     cond_name_label = ttk.Label(mission_editor_window, text="Cond Name:")
-    prereq_ids_label = ttk.Label(mission_editor_window, text="Condition Unlocks:")
-    world_state_ids_label = ttk.Label(mission_editor_window, text="World State Unlocks:")
+    prereq_ids_label = ttk.Label(mission_editor_window, text="Condition Unlocks (comma delimited):")
+    world_state_ids_label = ttk.Label(mission_editor_window, text="World State Unlocks (state:active_flag, comma delimited):")
 
     # Widget Gridding:
 
